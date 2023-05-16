c6db989617ea3454ba10f9d2ff74e9ae01c249e1
Sainishchai <89191791+Sainishchai@users.noreply.github.com>
2023-05-16 14:20:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@ import os
 
 
 class Config(object):
-    API_HASH = os.environ.get("API_HASH" ,"")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN" ,"")
-    TELEGRAM_API = os.environ["TELEGRAM_API" ,""]
-    OWNER = os.environ.get("OWNER ,""")
+    API_HASH = os.environ.get("API_HASH" ,"152e94d23ca0b3cc96a2c1278b967ca4")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN" ,"6039043446:AAHf8ODju2FpNFBXj7v2-MwC0HxpLAu_0wU")
+    TELEGRAM_API = os.environ["TELEGRAM_API" ,"21525441"]
+    OWNER = os.environ.get("OWNER , "")
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME" ,"")
     PASSWORD = os.environ.get("PASSWORD" ,"")
     DATABASE_URL = os.environ.get("DATABASE_URL" ,"")
