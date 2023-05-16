5632543a53fe2214c2a0e830a15feeca6c5174b4
Sainishchai <89191791+Sainishchai@users.noreply.github.com>
2023-05-16 14:52:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH")
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     TELEGRAM_API = os.environ["TELEGRAM_API"]
-    OWNER = os.environ.get("OWNER , "1172763005")
+    OWNER = os.environ.get("OWNER")
     OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
     PASSWORD = os.environ.get("PASSWORD")
     DATABASE_URL = os.environ.get("DATABASE_URL")
